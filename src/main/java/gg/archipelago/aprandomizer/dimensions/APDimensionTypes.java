ce316be116034f4e5f3fc72ed822fabd63050552
coehlrich <coehlrich@users.noreply.github.com>
2025-09-19 00:15:19 +1200

    Add immune to zombification tags to mobs from bastions and remove overworld dimension type overrides

--- src/main/java/gg/archipelago/aprandomizer/dimensions/APDimensionTypes.java
+++ src/main/java/gg/archipelago/aprandomizer/dimensions/APDimensionTypes.java
@@ -3,7 +3,6 @@ package gg.archipelago.aprandomizer.dimensions;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantInt;
-import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 import net.minecraft.world.level.dimension.DimensionType;
 
@@ -12,28 +11,6 @@ import java.util.OptionalLong;
 
 public class APDimensionTypes {
     public static void bootstrap(BootstrapContext<DimensionType> context) {
-        context.register(BuiltinDimensionTypes.OVERWORLD,
-                new DimensionType(
-                        OptionalLong.empty(),
-                        true,
-                        false,
-                        false,
-                        true,
-                        1,
-                        true,
-                        false,
-                        -64,
-                        384,
-                        384,
-                        BlockTags.INFINIBURN_OVERWORLD,
-                        BuiltinDimensionTypes.OVERWORLD_EFFECTS,
-                        0,
-                        Optional.of(192),
-                        new DimensionType.MonsterSettings(
-                                true,
-                                true,
-                                UniformInt.of(0, 7),
-                                0)));
         context.register(BuiltinDimensionTypes.NETHER,
                 new DimensionType(
                         OptionalLong.of(18000),
@@ -56,27 +33,5 @@ public class APDimensionTypes {
                                 true,
                                 ConstantInt.of(11),
                                 15)));
-        context.register(BuiltinDimensionTypes.OVERWORLD_CAVES,
-                new DimensionType(
-                        OptionalLong.empty(),
-                        true,
-                        true,
-                        false,
-                        true,
-                        1,
-                        true,
-                        false,
-                        -64,
-                        384,
-                        384,
-                        BlockTags.INFINIBURN_OVERWORLD,
-                        BuiltinDimensionTypes.OVERWORLD_EFFECTS,
-                        0,
-                        Optional.of(192),
-                        new DimensionType.MonsterSettings(
-                                true,
-                                true,
-                                UniformInt.of(0, 7),
-                                0)));
     }
 }
